51979e7df77350a1c9a9e6450755d81e36959bd8
Arun Jayaraman <141009715+arunkabish1@users.noreply.github.com>
2024-04-05 11:50:20 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from '/home/rog/portfolio/portfolio/src/assets/logo.png';
 
 export const openGmail = () => {
   const email = 'arunkabish@gmail.com';
@@ -11,9 +10,7 @@ const Header = () => {
   return (
     <div style={{ position: 'fixed', width: '96%' }} className="header-container">
       <nav className='flex flex-row p-4 '>
-        <a href="#">
-          <img src={Logo} alt="arunkabish" className="p-1 font-bold h-8" />
-        </a>
+      
         <button className="ml-auto hover:bg-slate-500 rounded-2xl p-1 font-bold" id='' onClick={openGmail}>Contact</button>
       </nav>
     </div>
