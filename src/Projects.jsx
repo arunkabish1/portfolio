e521c5353719bdd542e75441b075eadc74658cb1
Arun Jayaraman <141009715+arunkabish1@users.noreply.github.com>
2024-04-05 11:55:58 +0530

    Update Projects.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -10,11 +10,7 @@ export const Projects = () => {
         href="https://lms-app-4x6h.onrender.com/index"
         className="flex flex-col items-center border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl"
       >
-       // <img
-        //  className="object-cover w-full rounded-t-lg h-96 md:h-40 md:w-48 md:rounded-none md:rounded-s-lg"
-        //  src={Node}
-        //  alt="nodejs task manager"
-       // />
+       
         <div className="flex flex-col justify-between p-4 leading-normal">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Learning Management System
